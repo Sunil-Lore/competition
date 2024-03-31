81e852324f7b3a0bbe24e14f4fdea97ca7b0df63
Sunil Kumar <sunil.lore@outlook.com>
2024-03-31 22:18:33 +0530

    link css etc

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php #index.php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Sunil-Lore/getcss@v0.1.0-alpha-09/dist/css/getcss.min.css"> 
+    <title>Make yourself ready for competitive exams.</title>
+</head>
+<body>
+    <div class="container-fluid">
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+</body>
+</html>
