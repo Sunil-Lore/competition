0f8e40bba6fc52c40b8f145018c2fd8281de87be
Sunil Kumar <sunil.lore@outlook.com>
2024-04-01 22:42:32 +0530

    updated font and added colors

--- index.php
+++ index.php
@@ -5,11 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Sunil-Lore/getcss@v0.1.0-alpha-09/dist/css/getcss.min.css"> 
+    <link rel="stylesheet" href="scss/custom.css"> 
     <title>Make yourself ready for competitive exams.</title>
 </head>
 <body>
     <div class="container-fluid">
-
+        <div style="height:250px;margin:auto;" class="shade-1">
+        <h1 class="lexend-100">Welcome</h1>
+        </div>
+        <div style="height:250px;margin:auto;" class="shade-2">
+        <h1>Welcome</h1>
+        </div>
+        <div style="height:250px;margin:auto;" class="shade-3">
+        <h1>Welcome</h1>
+        </div>
 
 
 
