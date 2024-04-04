de4432492b14760c3b7856c28e415f80203f2c0a
Sunil Kumar <sunil.lore@outlook.com>
2024-04-04 23:13:00 +0530

    work on shades

---
+++ blocks.php
@@ -0,0 +1,59 @@
+<?php #index.php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Sunil-Lore/getcss@v0.1.0-alpha-09/dist/css/getcss.min.css"> 
+    <link rel="stylesheet" href="scss/custom.css"> 
+    <title>Make yourself ready for competitive exams.</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row ">
+                <div class="white-on-black p-10-px h-150-px">
+                    <p class="fs-28 txt-center">
+                        White on black
+                        <br />
+                        <a href="">Welcome</a>
+                    </p>
+                </div>
+        </div>
+        <div class="row ">
+                <div class="white-on-darkblue p-10-px h-150-px">
+                    <p class="fs-28 txt-center">White on dark blue</p>
+                </div>
+        </div>
+        <div class="row ">
+                <div class="white-on-normalblue p-10-px h-150-px">
+                    <p class="fs-28 txt-center">white on normal blue</p>
+                </div>
+        </div>
+
+        <div class="row ">
+                <div class="black-on-lightblue p-10-px h-150-px">
+                    <p class="fs-28 txt-center">black on light blue</p>
+                </div>
+        </div>
+        <div class="row ">
+                <div class="black-on-skyblue p-10-px h-150-px">
+                    <p class="fs-28 txt-center">black on sky blue</p>
+                </div>
+        </div>
+        <div class="row ">
+                <div class="black-on-white p-10-px h-150-px">
+                    <p class="fs-28 txt-center">black on white</p>
+                </div>
+        </div>
+
+
+
+
+
+
+
+
+
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -10,54 +10,7 @@
 </head>
 <body>
     <div class="container-fluid">
-    <div style="height:125px;margin:auto;" class="black-shade-1">
-        <p class="fs-58">Light blue on black</p>
-        </div>
-        <div style="height:125px;margin:auto;" class="black-shade-2">
-        <p class="fs-58">sky blue on black</p>
-        </div>
-        <div style="height:125px;margin:auto;" class="black-shade-3">
-        <p class="fs-58">white on black</p>
-        </div>
-        <div style="height:125px;margin:auto;" class="dark-shade-1">
-        <p class="fs-58">light blue on dark blue</p>
-        </div>
-        <div style="height:125px;margin:auto;" class="dark-shade-2">
-        <p class="fs-58">sky blue on dark blue</p>
-        </div>
-        <div style="height:125px;margin:auto;" class="dark-shade-3">
-        <p class="fs-58">White on dark blue</p>
-        </div>
-        <div style="height:125px;margin:auto;" class="normal-shade-1">
-        <p class="fs-58">sky blue on normal blue</p>
-        </div>
-        <div style="height:125px;margin:auto;" class="normal-shade-2">
-        <p class="fs-58">white on normal blue</p>
-        </div>
-        <div style="height:125px;margin:auto;" class="light-shade-1">
-        <p class="fs-58">black on light blue</p>
-        </div>
-        <div style="height:125px;margin:auto;" class="light-shade-2">
-        <p class="fs-58">dark blue on light blue</p>
-        </div>
-        <div style="height:125px;margin:auto;" class="sky-shade-1">
-        <p class="fs-58">black on sky blue</p>
-        </div>
-        <div style="height:125px;margin:auto;" class="sky-shade-2">
-        <p class="fs-58">dark blue on sky blue</p>
-        </div>
-        <div style="height:125px;margin:auto;" class="sky-shade-3">
-        <p class="fs-58">normal blue on sky blue</p>
-        </div>
-        <div style="height:125px;margin:auto;" class="white-shade-1">
-        <p class="fs-58">black on white</p>
-        </div>
-        <div style="height:125px;margin:auto;" class="white-shade-2">
-        <p class="fs-58">dark blue on white</p>
-        </div>
-        <div style="height:125px;margin:auto;" class="white-shade-3">
-        <p class="fs-58">normal blue on white</p>
-        </div>
+
 
 
 
